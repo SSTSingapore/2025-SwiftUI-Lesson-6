b834f9994e74ec44fc8b767e5e528f5b57e51c20
Stanley Sim <sim_yong_seng_stanley@sst.edu.sg>
2025-06-26 21:47:09 +0800

    Initial commit

---
+++ 2025-SwiftUI-Lesson-6/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2025-SwiftUI-Lesson-6
+//
+//  Created by Stanley Sim on 26/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2025-SwiftUI-Lesson-6/_025_SwiftUI_Lesson_6App.swift
@@ -0,0 +1,17 @@
+//
+//  _025_SwiftUI_Lesson_6App.swift
+//  2025-SwiftUI-Lesson-6
+//
+//  Created by Stanley Sim on 26/6/25.
+//
+
+import SwiftUI
+
+@main
+struct _025_SwiftUI_Lesson_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
