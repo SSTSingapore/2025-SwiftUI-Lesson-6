a9fffbdb7588e9e92834be69e43faa5e9cf3ea1f
Stanley Sim <sim_yong_seng_stanley@sst.edu.sg>
2025-06-26 22:09:29 +0800

    Part-3

--- 2025-SwiftUI-Lesson-6/ContentView.swift
+++ 2025-SwiftUI-Lesson-6/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedRow: Int = 0
+    @State private var pokemon = PokemonModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("Pick a Pokemon").font(.headline)
+                Picker("Pokemon", selection: $selectedRow) {
+                    ForEach(0..<pokemon.names.count, id: \.self) { index in
+                        Text(pokemon.names[index]).tag(index)
+                    }
+                }.pickerStyle(.wheel)
+                
+                Spacer()
+                
+                Text("Type: \(pokemon.types[selectedRow])")
+                
+                Spacer()
+                
+                NavigationLink("More info about \(pokemon.names[selectedRow])") {
+                    PokemonView(selectedRow: selectedRow, pokemon: pokemon)
+                        .navigationTitle(pokemon.names[selectedRow])
+                }
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
